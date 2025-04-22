95be177a31eacb99f8f30d4d7f30d61726423ff2
User <muradabbsov280@gmail.com>
2025-04-22 23:41:35 +0400

    Aded ReactRouterModuleCss

---
+++ 26-reactrouterrodulecss/src/App.jsx
@@ -0,0 +1,25 @@
+import Header from './components/header/Header'
+import Present from './components/present/Present'
+import Features from `./components/features/Features`
+import Pay from './components/pay/Pay'
+import Customer from './components/customer/Customer'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <div>
+      
+    <Header/>
+    <Present/>
+   <Features/>
+   <Pay/>
+   <Customer/>
+   <Contact/>
+   <Footer/>
+
+    </div>
+  )
+}
+
+export default App
